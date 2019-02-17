0c28755635521141cecb7c4b56a0c3cbdb40cff2
Matt Galvin <mgalvin@endpoint.com>
2019-02-17 19:05:57 +0100

    Fix loading Auth plug in router

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,16 +4,11 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-
-      conn ->
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
@@ -22,7 +17,7 @@ defmodule RumblWeb.UserController do
   end
 
   def new(conn, _params) do
-    changeset = Accounts.change_registration(%User{}, %{})
+    changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
@@ -39,8 +34,8 @@ defmodule RumblWeb.UserController do
     end
   end
 
-  defp authenticate(conn) do
-    if conn.assigns[:current_user] do
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
       conn
     else
       conn
