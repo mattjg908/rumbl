ba3752ece9572699c262d2e67552abb06dfbfc4f
Matt Galvin <matt.galvin@dockyard.com>
2019-03-22 22:03:51 +0100

    Add tag and describe block to video tests

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,14 +1,25 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
+  import Phoenix.Controller
 
+  alias RumblWeb.Router.Helpers, as: Routes
   alias Rumbl.Accounts
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    user = user_id && Accounts.get_user(user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        conn
+
+      user = user_id && Accounts.get_user(user_id) ->
+        assign(conn, :current_user, user)
+
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
@@ -22,7 +33,6 @@ defmodule RumblWeb.Auth do
     configure_session(conn, drop: true)
   end
 
-  # should this be called login_by_username_and_pass
   def login_by_email_and_pass(conn, email, given_pass) do
     case Accounts.authenticate_by_email_and_pass(email, given_pass) do
       {:ok, user} -> {:ok, login(conn, user)}
@@ -30,9 +40,6 @@ defmodule RumblWeb.Auth do
       {:error, :not_found} -> {:error, :not_found, conn}
     end
   end
-
-  import Phoenix.Controller
-  alias RumblWeb.Router.Helpers, as: Routes
 
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
